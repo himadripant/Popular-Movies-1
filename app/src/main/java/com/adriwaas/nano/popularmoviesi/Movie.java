3a1e524784854ff8f6cd8197e6f9105a3291b8c7
Himadri Pant <himadri.pant@pactera.com.au>
2016-01-18 19:56:50 +1100

    initial frame

---
+++ app/src/main/java/com/adriwaas/nano/popularmoviesi/Movie.java
@@ -0,0 +1,96 @@
+package com.adriwaas.nano.popularmoviesi;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by Himadri Pant on 28/12/2015.
+ */
+public class Movie implements Parcelable {
+
+    String id;
+    String title;
+    String originalTitle;
+    String posterPath;
+    boolean isAdult;
+    String overview;
+    String releaseDate;
+    String originalLanguage;
+    String backdropPath;
+    double popularity;
+    int voteCount;
+    boolean hasVideo;
+    float voteAvg;
+    int[] genreIds;
+
+    public Movie(String id, String title, String originalTitle, String posterPath, boolean isAdult,
+                 String overview, String releaseDate, String originalLanguage, String backdropPath,
+                 double popularity, int voteCount, boolean hasVideo, float voteAvg, int[] genreIds) {
+        this.id = id;
+        this.title = title;
+        this.originalTitle = originalTitle;
+        this.posterPath = posterPath;
+        this.isAdult = isAdult;
+        this.overview = overview;
+        this.releaseDate = releaseDate;
+        this.originalLanguage = originalLanguage;
+        this.backdropPath = backdropPath;
+        this.popularity = popularity;
+        this.voteCount = voteCount;
+        this.hasVideo = hasVideo;
+        this.voteAvg = voteAvg;
+        this.genreIds = genreIds;
+    }
+
+    private Movie(Parcel in) {
+        this.id = in.readString();
+        this.title = in.readString();
+        this.originalTitle = in.readString();
+        this.posterPath = in.readString();
+        this.isAdult = in.readByte() == 1;
+        this.overview = in.readString();
+        this.releaseDate = in.readString();
+        this.originalLanguage = in.readString();
+        this.backdropPath = in.readString();
+        this.popularity = in.readDouble();
+        this.voteCount = in.readInt();
+        this.hasVideo = in.readByte() == 1;
+        this.voteAvg = in.readFloat();
+        in.readIntArray(genreIds);
+    }
+
+    public static final Creator<Movie> CREATOR = new Creator<Movie>() {
+        @Override
+        public Movie createFromParcel(Parcel in) {
+            return new Movie(in);
+        }
+
+        @Override
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(id);
+        dest.writeString(title);
+        dest.writeString(originalTitle);
+        dest.writeString(posterPath);
+        dest.writeByte((byte) (isAdult ? 1 : 0));
+        dest.writeString(overview);
+        dest.writeString(releaseDate);
+        dest.writeString(originalLanguage);
+        dest.writeString(backdropPath);
+        dest.writeDouble(popularity);
+        dest.writeInt(voteCount);
+        dest.writeByte((byte) (hasVideo ? 1 : 0));
+        dest.writeFloat(voteAvg);
+        dest.writeIntArray(genreIds);
+    }
+}
